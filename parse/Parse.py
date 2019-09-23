503df7a723bed882b8d56aa4300ad8cb33d45d27
Hunter Gregory <hlg16@duke.edu>
2019-09-23 02:01:15 -0400

    writing output to file now, fixed multiple bugs, working with script calls

--- parse/Parse.py
+++ parse/Parse.py
@@ -1,11 +1,10 @@
 import requests
 import sys
-from tqdm import trange
 from scan.Scanner import *
 from latin import WordFactory
 
-REPLACE_TAG_REGEXS = [r"<p>", r"</p>", r"<br>", r"<font size=\d+>[\d\w ]+</font>"]
-
+REPLACE_TAG_REGEXS = [r"<p>", r"</p>", r"<font size=\d+>[\d\w ]+</font>"]
+BREAK_REGEX = r"<br>"
 PUNCTUATION_REGEX = r"[\[\]\.,\/#!\?$%\^&\*;:{}=\-_`~()\"'(&#151;)]"
 FUNKY_SPACE_REGEX = r"&nbsp;"
 
@@ -21,7 +20,7 @@ def get_whitakers_url(word):
 
 
 def get_words_in_line(line):
-    return re.split(r"\s", line)
+    return re.split(r"\s+", line)
 
 
 def parse_lines(url):
@@ -32,12 +31,12 @@ def parse_lines(url):
     if not body_match:
         return False
     paragraphs = body_match.group(1)
+    paragraphs = re.sub(BREAK_REGEX, "\n", paragraphs, flags=re.I)
     for regex in REPLACE_TAG_REGEXS:
         paragraphs = re.sub(regex, "", paragraphs, flags=re.I)
     paragraphs = re.sub(FUNKY_SPACE_REGEX, "", paragraphs)
     paragraphs = re.sub(PUNCTUATION_REGEX, " ", paragraphs)
     scanner = Scanner(text=paragraphs)
-
     if scanner.current_contains(r"\w"):
         lines.append(get_words_in_line(scanner.current))
     while scanner.has_next():
@@ -61,8 +60,10 @@ def search_for_word(word):
 
 
 def output_vocab(word_set):
-    for word in word_set:  # FIXME output to file
-        print(word)
+    with open("vocab.txt", "w+") as file:
+        for word in word_set:
+            file.write(str(word))
+            file.write("\n")
 
 
 def main(url, line_start=0, line_end=-1):
@@ -72,18 +73,16 @@ def main(url, line_start=0, line_end=-1):
         return
 
     if line_end < 0 or line_start == line_end:
-        lines = lines[line_start:]
+        lines = lines[line_start - 1:]
     else:
-        lines = lines[line_start:line_end]
-
+        lines = lines[line_start - 1:line_end - 1]
     scanner = Scanner()
     word_factory = WordFactory.WordFactory()
-    latin_words = set()
-
-    for k in trange(len(lines)):
-        if k < line_start or (line_end != -1 and k > line_end):
-            continue
+    latin_words = []
+    for k in range(len(lines)):
         for word in lines[k]:
+            if word == '':
+                continue
             word_text = search_for_word(word)
             if not word_text:
                 continue
@@ -94,8 +93,8 @@ def main(url, line_start=0, line_end=-1):
                 if not scanner.current_contains(r"\[XXX\w*\]"):
                     continue
                 latin_word = word_factory.get_word(previous, scanner.current, scanner.get_next())
-                if latin_word:
-                    latin_words.add(latin_word)
+                if latin_word and latin_word not in latin_words:
+                    latin_words.append(latin_word)
 
     # TODO see if defs already in a list of known latin
     output_vocab(latin_words)
@@ -106,4 +105,7 @@ if __name__ == '__main__':
     book_num = int(sys.argv[1])
     start = int(sys.argv[2])
     end = int(sys.argv[3])
+    # book_num = 10
+    # start = 1
+    # end = 10
     main(get_eclogues_url(book_num), line_start=start, line_end=end)
